63da5befcb0f937974d8b2b4dbb150db0629d818
Nikita Ermakov <sh1r4s3@mail.si-head.nl>
2022-08-03 03:54:55 +0300

    Add IPv6 support
    - Get rid of NSWIZARD_N env var. Calculate the number of entries by
      enumerating from 0 to the last available.
    
    - Use ';' semi-colon as separator instead of ':' colon.
    
    - New NSWIZARD_NETNS_IPV6 env var allows to override bind IPv6
      addrs. Example:
      NSWIZARD_NETNS_IPV6="::1;9050;/var/run/netns/kek"

--- nswizard/nswizard_lib.c
+++ nswizard/nswizard_lib.c
@@ -21,8 +21,9 @@
 #include <arpa/inet.h>
 
 #define INLOG_NAME "libnswizard"
-#define ENV_NUM_LISTNERS "NSWIZARD_N"
 #define ENV_LISTNER "NSWIZARD_NETNS"
+#define ENV_LISTNER_IPV6 "NSWIZARD_NETNS_IPV6"
+#define ENV_SEPARATOR ';'
 
 // Emit log message
 #define ERR(format, ...) \
@@ -51,9 +52,10 @@
 #endif
 
 struct netns {
+    unsigned char ip[sizeof(struct in6_addr)];
     int fd;
+    sa_family_t family; // AF_INET or AF_INET6
     in_port_t port;
-    struct in_addr ip;
 };
 
 static int (*bind_orig)(int, const struct sockaddr *, socklen_t) = NULL;
@@ -61,35 +63,69 @@ static int ns_def_fd = 0;
 static int netns_size = 0;
 static struct netns *ns = NULL;
 
+static void add_netns(char *ip, sa_family_t family) {
+    char *port = NULL, *netns_path = NULL;
+    port = strchr(ip, ENV_SEPARATOR);
+    if (port) netns_path = strchr(port + 1, ENV_SEPARATOR);
+    if (!port || !netns_path) {
+        DEBUG("%s can't parse %s", __func__, ip);
+        return;
+    }
+    *port++ = '\0';
+    *netns_path++ = '\0';
+
+    inet_pton(family, ip, ns[netns_size].ip);
+    ns[netns_size].port = atoi(port);
+    ns[netns_size].fd = open(netns_path, 0);
+    ns[netns_size].family = family;
+    DEBUG("%s adding port=%d ip=%s(0x%x), netns=%d", __func__, ns[netns_size].port, ip, *((int *)ns[netns_size].ip), ns[netns_size].fd);
+    ++netns_size;
+}
+
 __attribute__((constructor))
 void wizard_lib_init() {
     DEBUG("%s", __func__);
+    char buf[255];
     bind_orig = dlsym(RTLD_NEXT, "bind");
     ns_def_fd = open("/proc/self/ns/net", 0);
 
-    char *nlsts_str = getenv(ENV_NUM_LISTNERS);
-    if (!nlsts_str) ERR(ENV_NUM_LISTNERS " does not defined");
-    // TODO: check for valid input in the env var
-    int nlsts = atoi(nlsts_str);
-    netns_size = nlsts;
-    ns = malloc(sizeof(ns)*nlsts); // TODO: check for NULL
-    char buf[255];
-    for (int i = 0; i < nlsts; ++i) {
+    int nssz = 0, nssz_ipv6 = 0;
+    // Scan for IPv4
+    while (1) {
+        sprintf(buf, ENV_LISTNER "_%d", nssz);
+        char *env = getenv(buf);
+        if (!env) break;
+        ++nssz;
+    }
+    // Scan for IPv6
+    while (1) {
+        sprintf(buf, ENV_LISTNER_IPV6 "_%d", nssz_ipv6);
+        char *env = getenv(buf);
+        if (!env) break;
+        ++nssz_ipv6;
+    }
+
+    DEBUG("found nssz=%d nssz_ipv6=%d", nssz, nssz_ipv6);
+    ns = malloc(sizeof(ns)*(nssz + nssz_ipv6)); // TODO: check for NULL
+    for (int i = 0; i < nssz; ++i) {
         sprintf(buf, ENV_LISTNER "_%d", i);
         DEBUG("%s buf is %s", __func__, buf);
         char *ip = getenv(buf);
         if (!ip) {
             DEBUG("%s not found", buf);
-            continue;
+            break;
         }
-        char *port = strchr(ip, ':');
-        char *netns_path = strchr(port + 1, ':');
-        *port++ = '\0';
-        *netns_path++ = '\0';
-
-        inet_aton(ip, &ns[i].ip);
-        ns[i].port = atoi(port);
-        ns[i].fd = open(netns_path, 0);
+        add_netns(ip, AF_INET);
+    }
+    for (int i = 0; i < nssz; ++i) {
+        sprintf(buf, ENV_LISTNER_IPV6 "_%d", i);
+        DEBUG("%s buf is %s", __func__, buf);
+        char *ip = getenv(buf);
+        if (!ip) {
+            DEBUG("%s not found", buf);
+            break;
+        }
+        add_netns(ip, AF_INET6);
     }
 }
 
@@ -120,14 +156,33 @@ static void switch_ns(int sockfd, int ns_fd) {
 
 int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
     DEBUG("%s enter with sockfd=%d", __func__, sockfd);
-    if (addrlen == sizeof(struct sockaddr_in)) {
-        const struct sockaddr_in *ipaddr = (const struct sockaddr_in *)addr;
-        for (int i = 0; i < netns_size; ++i) {
-            DEBUG("%s testing %d:%d against %d:%d", __func__,
-                  ipaddr->sin_addr.s_addr, ntohs(ipaddr->sin_port),
-                  ns[i].ip.s_addr, ns[i].port);
-            if (ns[i].ip.s_addr == ipaddr->sin_addr.s_addr && ns[i].port == ntohs(ipaddr->sin_port))
-                switch_ns(sockfd, ns[i].fd);
+    sa_family_t family;
+    switch (addrlen) {
+        case sizeof(struct sockaddr_in):
+            DEBUG("%s family AF_INET", __func__);
+            family = AF_INET;
+            break;
+        case sizeof(struct sockaddr_in6):
+            DEBUG("%s family AF_INET6", __func__);
+            family = AF_INET6;
+            break;
+        default:
+            DEBUG("%s skip sockfd=%d", __func__, sockfd);
+            return bind_orig(sockfd, addr, addrlen);
+    }
+
+    const struct sockaddr_in *ipaddr = (const struct sockaddr_in *)addr;
+    for (int i = 0; i < netns_size; ++i) {
+        DEBUG("%s checking i=%d", __func__, i);
+        if (ns[i].family != family || ns[i].port != ntohs(ipaddr->sin_port)) continue;
+        void *ip = family == AF_INET ?
+                           (void *)&((const struct sockaddr_in *)addr)->sin_addr :
+                           (void *)&((const struct sockaddr_in6 *)addr)->sin6_addr;
+        DEBUG("%s cmp 0x%x with 0x%x", __func__, *((int *)ns[i].ip), *((int *)ip));
+        if (memcmp((void *)ns[i].ip, ip, family == AF_INET ? sizeof(struct in_addr) : sizeof(struct in6_addr)) == 0) {
+            DEBUG("%s switching netns to %d", __func__, ns[i].fd);
+            switch_ns(sockfd, ns[i].fd);
+            break;
         }
     }
     DEBUG("%s exiting with sockfd=%d", __func__, sockfd);
