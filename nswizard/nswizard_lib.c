370648fcf94621e49a2cef86065a8a6cfd96deef
Nikita Ermakov <sh1r4s3@mail.si-head.nl>
2022-11-15 06:14:58 +0200

    Allow to separate L4 protocols to different netns

--- nswizard/nswizard_lib.c
+++ nswizard/nswizard_lib.c
@@ -51,10 +51,17 @@
 #  define DEBUG(...)
 #endif
 
+typedef enum {
+    L4_PROTOCOL_TCP,
+    L4_PROTOCOL_UDP,
+    L4_PROTOCOL_UNK
+} L4_PROTOCOLS;
+
 struct netns {
     unsigned char ip[sizeof(struct in6_addr)];
     int fd;
     sa_family_t family; // AF_INET or AF_INET6
+    L4_PROTOCOLS proto; // TCP, UDP, cosmoshroom radiation
     in_port_t port;
 };
 
@@ -67,18 +74,32 @@ static void add_netns(char *ip, sa_family_t family) {
     char *port = NULL, *netns_path = NULL;
     port = strchr(ip, ENV_SEPARATOR);
     if (port) netns_path = strchr(port + 1, ENV_SEPARATOR);
-    if (!port || !netns_path) {
+    if (!port || !netns_path) { // Mandatory fields
         DEBUG("%s can't parse %s", __func__, ip);
         return;
     }
     *port++ = '\0';
     *netns_path++ = '\0';
+    // An optional field for level 4 protocols accocording to the OSI
+    char *l4_proto_str = strchr(netns_path + 1, ENV_SEPARATOR);
+    L4_PROTOCOLS l4_proto = L4_PROTOCOL_UNK;
+    if (l4_proto_str) {
+        *l4_proto_str++ = '\0';
+        if (strcmp(l4_proto_str, "tcp") == 0) {
+            l4_proto = L4_PROTOCOL_TCP;
+        } else if (strcmp(l4_proto_str, "udp") == 0) {
+            l4_proto = L4_PROTOCOL_UDP;
+        }
+
+        DEBUG("l4_proto_str=%s l4_proto=%d", l4_proto_str, l4_proto);
+    }
 
     inet_pton(family, ip, ns[netns_size].ip);
     ns[netns_size].port = atoi(port);
     ns[netns_size].fd = open(netns_path, 0);
     ns[netns_size].family = family;
-    DEBUG("%s adding port=%d ip=%s(0x%x), netns=%d", __func__, ns[netns_size].port, ip, *((int *)ns[netns_size].ip), ns[netns_size].fd);
+    ns[netns_size].proto = l4_proto;
+    DEBUG("%s adding port=%d ip=%s(0x%x), netns=%d, proto=%d", __func__, ns[netns_size].port, ip, *((int *)ns[netns_size].ip), ns[netns_size].fd, ns[netns_size].proto);
     ++netns_size;
 }
 
@@ -106,7 +127,11 @@ void wizard_lib_init() {
     }
 
     DEBUG("found nssz=%d nssz_ipv6=%d", nssz, nssz_ipv6);
-    ns = malloc(sizeof(struct netns)*(nssz + nssz_ipv6)); // TODO: check for NULL
+    ns = malloc(sizeof(struct netns)*(nssz + nssz_ipv6));
+    if (!ns) {
+        ERR("The Void has opened and a null pointer emerged in our world");
+        return;
+    }
     for (int i = 0; i < nssz; ++i) {
         sprintf(buf, ENV_LISTNER "_%d", i);
         DEBUG("%s buf is %s", __func__, buf);
@@ -139,18 +164,12 @@ void wizard_lib_deinit() {
     free(ns);
 }
 
-static void switch_ns(int sockfd, int ns_fd, sa_family_t family) {
+static void switch_ns(int sockfd, int ns_fd, sa_family_t family, int type) {
     int so_options_def[] = {SO_REUSEADDR, SO_REUSEPORT, SO_KEEPALIVE, SO_DONTROUTE};
     int ip_options_def[] = {IP_TRANSPARENT};
     int ipv6_options_def[] = {IPV6_TRANSPARENT};
     int tcp_options_def[] = {TCP_NODELAY, TCP_CORK, TCP_DEFER_ACCEPT, TCP_QUICKACK};
-    int type;
-    socklen_t length = sizeof(int);
     int err;
-    err = getsockopt(sockfd, SOL_SOCKET, SO_TYPE, &type, &length);
-    if (err < 0) {
-        WARN("can't get socket type, errno=%d", errno);
-    }
 
     // Get CLOEXEC and NONBLOCK fd flags
     int flags = fcntl(sockfd, F_GETFD);
@@ -262,8 +281,30 @@ int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
                            (void *)&((const struct sockaddr_in6 *)addr)->sin6_addr;
         DEBUG("%s cmp 0x%x with 0x%x", __func__, *((int *)ns[i].ip), *((int *)ip));
         if (memcmp((void *)ns[i].ip, ip, family == AF_INET ? sizeof(struct in_addr) : sizeof(struct in6_addr)) == 0) {
+            int type;
+            socklen_t length = sizeof(int);
+            // Get SOCK type
+            if (getsockopt(sockfd, SOL_SOCKET, SO_TYPE, &type, &length) < 0) {
+                WARN("can't get socket type, errno=%d", errno);
+            }
+            if (ns[i].proto != L4_PROTOCOL_UNK) {
+                int good = 0;
+                switch (ns[i].proto) {
+                    case L4_PROTOCOL_TCP:
+                        good = type & SOCK_STREAM;
+                        break;
+                    case L4_PROTOCOL_UDP:
+                        good = type & SOCK_DGRAM;
+                        break;
+                }
+                if (!good) {
+                    DEBUG("wrong l4 proto=%d type=%d", ns[i].proto, type);
+                    continue;
+                }
+            }
+
             DEBUG("%s switching netns to %d", __func__, ns[i].fd);
-            switch_ns(sockfd, ns[i].fd, family);
+            switch_ns(sockfd, ns[i].fd, family, type);
             break;
         }
     }
