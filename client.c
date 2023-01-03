sh1r4s3/runns

commit 54b1d784165c4001d7b1ad5704d1cf86bc584a51

Add --forward-port option to move a socket to different netns

client.c
@@ -6,13 +6,25 @@
  */
 
 #include "runns.h"
+#include <arpa/inet.h>
+#include <limits.h>
 
-// Emit error message and exit
+#define CLIENT_NAME "runnsctl"
+#define ENV_SEPARATOR ';'
+
+void cleanup();
+
 #define ERR(format, ...) \
       fprintf(stderr, "client.c:%d / errno=%d / " format "\n", __LINE__, errno, ##__VA_ARGS__); \
       ret = EXIT_FAILURE;
 
 enum wide_opts {OPT_SET_NETNS = 0xFF01, OPT_SOCKET = 0xFF02};
+enum operation_mode {OP_MODE_UNK, OP_MODE_NETNS, OP_MODE_FWD_PORT};
+
+static int netns_size = 0;
+static struct netns *ns_head = NULL;
+static enum operation_mode op_mode = OP_MODE_UNK;
+static int sockfd = 0;
 
 struct option opts[] =
 {
@@ -22,6 +34,7 @@ struct option opts[] =
   { .name = "stop", .has_arg = 0, .flag = 0, .val = 's' },
   { .name = "list", .has_arg = 0, .flag = 0, .val = 'l' },
   { .name = "create-ptms", .has_arg = 0, .flag = 0, .val = 't' },
+  { .name = "forward-port", .has_arg = 1, .flag = 0, .val = 'f' },
   { .name = "set-netns", .has_arg = 1, .flag = 0, .val = OPT_SET_NETNS },
   { .name = "socket", .has_arg = 1, .flag = 0, .val = OPT_SOCKET },
   { 0, 0, 0, 0 }
@@ -33,19 +46,106 @@ void
 help_me()
 {
   const char *hstr = \
-"client [options]\n"                                            \
-"Options:\n"                                                    \
-"-h|--help             help\n"                                  \
-"-s|--stop             stop daemon (only root)\n"               \
-"-l|--list             list childs\n"                           \
-"-p|--program <path>   program to run in desired netns\n"       \
-"-t|--create-ptms      create control terminal\n"               \
-"--set-netns <path>    network namespace to switch\n"           \
-"--socket <path>       path to the runns socket\n"              \
+"client [options]  \n"                                                \
+"Options:  \n"                                                        \
+"-h|--help             help  \n"                                      \
+"-s|--stop             stop daemon (only root)  \n"                   \
+"-l|--list             list childs  \n"                               \
+"-p|--program <path>   program to run in desired netns  \n"           \
+"-t|--create-ptms      create control terminal  \n"                   \
+"-f|--forward-port     <ip>;<port>;<netns path>;<proto><ip family>\n" \
+"                      <ip family> could be 4 or 6\n"                 \
+"                      <netns path> path to the netns fd\n"           \
+"--set-netns <path>    network namespace to switch  \n"               \
+"--socket <path>       path to the runns socket  \n"                  \
 "-v|--verbose          be verbose\n";
 
   puts(hstr);
   exit(EXIT_SUCCESS);
+}
+
+// TODO: check whether we need static here or it is redundant?
+static inline void parse_l4_proto(char *l4_proto_str, L4_PROTOCOLS *l4_proto, sa_family_t *family) {
+    // Basic checks and calc the size of the str
+    if (!l4_proto_str || !l4_proto || !family) {
+        ERR("NULL slipped in 0x%x 0x%x 0x%x", l4_proto_str, l4_proto, family);
+    }
+    *l4_proto_str++ = '\0';
+    *family = AF_INET;
+    *l4_proto = L4_PROTOCOL_UNK;
+    size_t l4_proto_str_sz = strlen(l4_proto_str);
+    if (l4_proto_str_sz != 4) {
+        WARN("L4 protocol has wrong size %d != 4. Skip", l4_proto_str_sz);
+        return;
+    }
+    // Get IP family
+    switch (l4_proto_str[3]) {
+        case '4':
+            *family = AF_INET;
+            break;
+        case '6':
+            *family = AF_INET6;
+            break;
+        default:
+            WARN("L4 protocol has wrong IP family %c (only 4 and 6 are allowed). Fallback to 4", l4_proto_str[3]);
+    }
+    // Get proto
+    if (strncmp(l4_proto_str, "tcp", 3) == 0) {
+        *l4_proto = L4_PROTOCOL_TCP;
+    } else if (strncmp(l4_proto_str, "udp", 3) == 0) {
+        *l4_proto = L4_PROTOCOL_UDP;
+    } else {
+        WARN("L4 protocol %s is not correct");
+    }
+
+    DEBUG("l4_proto_str=%s l4_proto=%d family=%d", l4_proto_str, l4_proto, family);
+}
+
+static void add_netns(char *ip) {
+    char *port = NULL, *netns_path = NULL;
+    port = strchr(ip, ENV_SEPARATOR);
+    if (port) netns_path = strchr(port + 1, ENV_SEPARATOR);
+    if (!port || !netns_path) { // Mandatory fields
+        DEBUG("%s can't parse %s", __func__, ip);
+        return;
+    }
+    *port++ = '\0';
+    *netns_path++ = '\0';
+    // An optional field for level 4 protocols accocording to the OSI
+    char *l4_proto_str = strchr(netns_path + 1, ENV_SEPARATOR);
+    L4_PROTOCOLS l4_proto = L4_PROTOCOL_UNK;
+    sa_family_t family = AF_UNSPEC;
+    parse_l4_proto(l4_proto_str, &l4_proto, &family);
+
+    // Create new
+    struct netns *ns = (struct netns *)malloc(sizeof(struct netns));
+    inet_pton(family, ip, ns->ip);
+    ns->port = atoi(port);
+    ns->netns = strndup(netns_path, PATH_MAX);
+    ns->family = family;
+    ns->proto = l4_proto;
+    ns->pnext = NULL;
+    DEBUG("%s adding port=%d ip=%s(0x%x), netns=%s, proto=%d", __func__, ns->port, ip, *((int *)ns->ip), ns->netns, ns->proto);
+    ++netns_size;
+    // Insert into list
+    if (ns_head) {
+      struct netns *p;
+      for (p = ns_head; p != NULL; p = p->pnext);
+      p->pnext = ns;
+    } else {
+      ns_head = ns;
+    }
+}
+
+void cleanup() {
+  for (struct netns *p = ns_head; p != NULL;) {
+    struct netns *ptmp = p->pnext;
+    free(p);
+    p = ptmp;
+  }
+
+  if (sockfd)
+    close(sockfd);
 }
 
 int
@@ -57,14 +157,10 @@ main(int argc, char **argv)
   const char *optstring = "hp:vslt";
   int opt, len;
   char verbose = 0;
-  int sockfd = 0;
   int ret = EXIT_SUCCESS;
 
   if (argc <= 1)
-  {
     ERR("For the help message try: runnsctl --help");
-    goto _exit;
-  }
 
   while ((opt = getopt_long(argc, argv, optstring, opts, 0)) != -1)
   {
@@ -86,34 +182,48 @@ main(int argc, char **argv)
       case 't':
         hdr.flag |= RUNNS_NPTMS;
         break;
+      case 'f':
+        if (op_mode == OP_MODE_NETNS) {
+            ERR("--forward-port and --set-netns mutually exclusive");
+        }
+        op_mode = OP_MODE_FWD_PORT;
+        add_netns(optarg);
+        break;
       case 'v':
         verbose = 1;
         break;
       case OPT_SET_NETNS:
+        if (op_mode == OP_MODE_FWD_PORT) {
+            ERR("--forward-port and --set-netns mutually exclusive");
+        }
+        op_mode = OP_MODE_NETNS;
         netns = optarg;
         hdr.netns_sz = strlen(netns) + 1;
         break;
       case OPT_SOCKET:
         len = strlen(optarg);
         if (len >= RUNNS_MAXLEN)
-        {
           ERR("Socket file name is too long > " STR_TOKEN(RUNNS_MAXLEN) "\n");
-          goto _exit;
-        }
         memcpy(addr.sun_path, optarg, len + 1);
         break;
       default:
         ERR("Wrong option: %c", (char)opt);
-        goto _exit;
     }
   }
 
+  // TODO
+#if 0
+  if (op_mode == OP_MODE_FWD_PORT) {
+    for (struct netns *p = ns_head; p != NULL; p = p->pnext) {
+      INFO("0x%x %s %s %d %d", *((int *)p->ip), p->netns, p->family == AF_INET ? "AF_INET" : "AF_INET6", p->proto, p->port);
+    }
+    exit(0);
+  }
+#endif
+
   // Not allow empty strings
   if (!hdr.flag && (!netns || !prog))
-  {
     ERR("Please check that you set network namespace and program");
-    goto _exit;
-  }
 
   // Output parameters in the case of verbose option
   if (verbose && !hdr.flag)
@@ -131,12 +241,14 @@ main(int argc, char **argv)
   if (sockfd == -1)
   {
     ERR("Something gone very wrong, socket = %d", sockfd);
-    goto _exit;
+    cleanup();
+    return ret;
   }
   if (connect(sockfd, (const struct sockaddr *)&addr, sizeof(addr)) == -1)
   {
     ERR("Can't connect to runns daemon");
-    goto _exit;
+    cleanup();
+    return ret;
   }
   // Calculate number of non-options
   hdr.args_sz = argc - optind;
@@ -147,8 +259,10 @@ main(int argc, char **argv)
   if (write(sockfd, (void *)&hdr, sizeof(hdr)) == -1)
     ERR("Can't send header to the daemon");
   // Stop daemon
-  if (hdr.flag & RUNNS_STOP)
-    goto _exit;
+  if (hdr.flag & RUNNS_STOP) {
+    cleanup();
+    return ret;
+  }
   // Print list of childs and exit
   if (hdr.flag & RUNNS_LIST)
   {
@@ -162,7 +276,8 @@ main(int argc, char **argv)
         ERR("Can't read child info from the daemon");
       printf("%d\n", child.pid);
     }
-    goto _exit;
+    cleanup();
+    return ret;
   }
 
   if (write(sockfd, (void *)prog, hdr.prog_sz) == -1 ||
@@ -198,8 +313,6 @@ main(int argc, char **argv)
   if (write(sockfd, &eof, sizeof(int)) == -1)
     ERR("Can't send EOF to the daemon");
 
-_exit:
-  if (sockfd)
-    close(sockfd);
+  cleanup();
   return ret;
 }

runns.h
@@ -20,6 +20,8 @@
 #include <string.h>
 #include <signal.h>
 #include <termios.h>
+#include <netinet/in.h>
+#include <fcntl.h>
 
 #define STR_TOKEN(x) #x
 
@@ -54,4 +56,21 @@ struct runns_child
   uid_t uid;
   pid_t pid;
 };
+
+// Structures for librunns
+typedef enum {
+    L4_PROTOCOL_UNK = 0,
+    L4_PROTOCOL_TCP,
+    L4_PROTOCOL_UDP
+} L4_PROTOCOLS;
+
+struct netns {
+    unsigned char ip[sizeof(struct in6_addr)];
+    char *netns;
+    sa_family_t family; // AF_INET or AF_INET6
+    L4_PROTOCOLS proto; // TCP, UDP, cosmoshroom radiation
+    in_port_t port;
+    struct netns *pnext;
+};
+
 #endif

commit 18582efdd57293c800b4ed90e05cc9a7e485ad35

Case insensitive cmp. Fix SIGSEGV.
Fixes: 54b1d784

client.c
@@ -111,12 +111,12 @@ static inline void parse_l4_proto(char *l4_proto_str, L4_PROTOCOLS *l4_proto, sa
             WARN("L4 protocol has wrong IP family %c (only 4 and 6 are allowed). Fallback to 4", l4_proto_str[3]);
     }
     // Get proto
-    if (strncmp(l4_proto_str, "tcp", 3) == 0) {
+    if (strncasecmp(l4_proto_str, "tcp", 3) == 0) {
         *l4_proto = L4_PROTOCOL_TCP;
-    } else if (strncmp(l4_proto_str, "udp", 3) == 0) {
+    } else if (strncasecmp(l4_proto_str, "udp", 3) == 0) {
         *l4_proto = L4_PROTOCOL_UDP;
     } else {
-        WARN("L4 protocol %s is not correct");
+        WARN("L4 protocol %s is not correct", l4_proto_str);
     }
 
     DEBUG("l4_proto_str=%s l4_proto=%d family=%d", l4_proto_str, l4_proto, family);