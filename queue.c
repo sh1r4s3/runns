3aefdaab10cec48756f977868d75a9ee2701e229
Nikita Ermakov <sh1r4s3@mail.si-head.nl>
2023-01-10 02:28:11 +0200

    POSIX mq wrapper and unit tests based on tau framework

---
+++ queue.c
@@ -0,0 +1,80 @@
+/*
+ * vim:et:sw=2:
+ *
+ * Copyright (c) 2023 Nikita (sh1r4s3) Ermakov <sh1r4s3@mail.si-head.nl>
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include "queue.h"
+
+struct queue *queue_open(const char *name) {
+  int exists;
+  struct queue *ptr = (struct queue *)malloc(sizeof(struct queue));
+  if (!ptr)
+    return NULL;
+
+  // Set a queue entry
+  struct mq_attr attr = {
+    .mq_flags = 0,
+    .mq_maxmsg = 16,
+    .mq_msgsize = 1024,
+    .mq_curmsgs = 0
+  };
+  ptr->name = strdup(name);
+  ptr->desc = mq_open(name, O_RDWR | O_CREAT, 0666, &attr);
+  if (ptr->desc == (mqd_t)-1)
+    return NULL;
+
+  return ptr;
+}
+
+QUEUE_RET queue_close(struct queue *queue) {
+  free(queue->name);
+  int ret = mq_close(queue->desc);
+  int _errno = errno;
+  free(queue);
+  errno = _errno;
+  return ret < 0 ? QUEUE_ERROR : QUEUE_OK;
+}
+
+QUEUE_RET queue_send(struct queue *queue, struct queue_msg *msgs) {
+  for (struct queue_msg *ptr = msgs; ptr != NULL; ptr = ptr->pnext) {
+    if (mq_send(queue->desc, ptr->msg, ptr->size, 0) < 0) {
+      return QUEUE_ERROR;
+    }
+  }
+  return QUEUE_OK;
+}
+
+struct queue_msg *queue_recv(struct queue *queue) {
+  struct mq_attr attr;
+  struct queue_msg *msg_head = NULL, **msg_tail = &msg_head;
+  if (mq_getattr(queue->desc, &attr) < 0)
+    return NULL;
+
+  while (attr.mq_curmsgs--) {
+    char *ptr = (char *)malloc(attr.mq_msgsize);
+    size_t sz = mq_receive(queue->desc, ptr, attr.mq_msgsize, NULL);
+    *msg_tail = (struct queue_msg *)malloc(sizeof(struct queue_msg));
+    (*msg_tail)->msg = ptr;
+    (*msg_tail)->size = sz;
+    (*msg_tail)->pnext = NULL;
+    msg_tail = &((*msg_tail)->pnext);
+  }
+  return msg_head;
+}
+
+QUEUE_RET queue_free_msgs(struct queue_msg *msgs) {
+  struct queue_msg *ptr = msgs, *ptr_next;
+  do {
+    ptr_next = ptr->pnext;
+    free(ptr->msg);
+    free(ptr);
+    ptr = ptr_next;
+  } while(ptr);
+}
---
+++ tests/test_queue.c
@@ -0,0 +1,56 @@
+/*
+ * vim:et:sw=2:
+ *
+ * Copyright (c) 2023 Nikita (sh1r4s3) Ermakov <sh1r4s3@mail.si-head.nl>
+ * SPDX-License-Identifier: MIT
+ */
+#include <errno.h>
+#include "tau/tau.h"
+#include "queue.h"
+
+TAU_MAIN();
+
+TEST(queue, open_close) {
+  const char qname[] = "/runnsqueue";
+  struct queue *q = queue_open(qname);
+  REQUIRE(q, "queue_open() returns NULL ptr");
+  QUEUE_RET ret = queue_close(q);
+  REQUIRE(ret == QUEUE_OK);
+}
+
+TEST(queue, send_recv) {
+  const char qname[] = "/runnsqueue";
+  const char test_msg[] = "test message";
+  pid_t pid = fork();
+  REQUIRE(pid >= 0, "fork failed");
+
+  if (pid == 0) { // child
+    struct queue *q = queue_open(qname);
+    REQUIRE(q, "queue_open() returns NULL ptr");
+
+    struct queue_msg msg = {0};
+    msg.msg = strdup(test_msg);
+    msg.size = sizeof(test_msg);
+    QUEUE_RET ret = queue_send(q, &msg);
+    CHECK(ret == QUEUE_OK, "Can't send message");
+    free(msg.msg);
+
+    ret = queue_close(q);
+    REQUIRE(ret == QUEUE_OK);
+  } else { // parent
+    struct queue *q = queue_open(qname);
+    REQUIRE(q, "queue_open() returns NULL ptr");
+
+    struct queue_msg *msgs = queue_recv(q);
+    CHECK(msgs != NULL, "Received NULL");
+    if (msgs != NULL) {
+      CHECK(strcmp(msgs->msg, test_msg) == 0, "Messages are not equal");
+      CHECK(msgs->pnext == NULL, "pnext is not NULL");
+      queue_free_msgs(msgs);
+    }
+
+    QUEUE_RET ret = queue_close(q);
+    REQUIRE(ret == QUEUE_OK);
+  }
+
+}
